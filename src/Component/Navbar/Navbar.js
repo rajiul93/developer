b6e74919e5efb31ada767e5b83430d5161c03245
rajiul93 <rajiulislam283@gmail.com>
2023-04-15 21:35:20 +0600

    upload

---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            {/* computer nav */}
+            <div>
+                <div><h2>I'm Juwel</h2></div>
+                <div></div>
+
+            </div>
+
+
+            {/* mobile nav */}
+            <div></div>
+        </div>
+    )
+}
+
+export default Navbar
