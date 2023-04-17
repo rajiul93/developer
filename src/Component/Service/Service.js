3b432a306c1fff85cecd5bfe46a9d16dbbd5a967
rajiul93 <rajiulislam283@gmail.com>
2023-04-17 23:18:42 +0600

    change

--- src/Component/Service/Service.js
+++ src/Component/Service/Service.js
@@ -7,23 +7,30 @@ function Service() {
     return (
         <section className='service'>
             <h1 className='text-center my-5'>Service I provide</h1>
-            <div className='service-pd-container'>
-                {
-                    Data.map((data, index) => {
-                        const {title, img, des} = data;
-                        return <div key={index} className='service-pd mx-auto d-block'>
-                            <div className='service-img-container'>
-                                <img className='service-img' src={img} alt="" />
-                            </div>
-                            <h2 className='fs-5'>{title}</h2>
-                            <p>{des}</p>
-                            <div className="position-relative">
-                            <button className='button button-1'>{title}</button>
-                            </div>
-                        </div>
 
-                    })
-                }
+
+            <div className="categories">
+                <div className="row">
+                    {
+                        Data.map((data, index) => {
+                            const { title, img, des } = data;
+
+                            return <div className="col-md-4 pd-container col-sm-6 mb-4 overflow-hidden  text-center ">
+                                <div className='bg-light'>
+                                    <div className='service-img-container'>
+                                        <img className='service-img img-fluid' src={img} alt="" />
+                                    </div>
+                                    <h1 className='fs-5 mt-2'>{title}</h1>
+                                    <p>{des}</p>
+                                    <div className="position-relative">
+                                        <button className='button'>{title}</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        })
+                    }
+                </div>
             </div>
         </section>
     )
