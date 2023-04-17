0a978877207e7a51386452741a98e8548643c071
rajiul93 <rajiulislam283@gmail.com>
2023-04-18 00:05:06 +0600

    new

---
+++ src/Component/WhoAmI/WhoAnI.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./WhoAnI.css"
+function WhoAnI() {
+    return (
+        <div className='bg-white'>
+            <div className='who-am-i '>
+                <div className='who-am-i-child'>
+                    <div className='who-am-i-1 mb-4'>
+                        <h2 className='fs-4'>Who am I</h2>
+                        <p>I am a full-time WordPress web developer with over six years of experience. With over 300 projects completed, I’m not only proficient with WordPress but a wide range of other CMS and related technologies such as Wix, Joomla, Drupal, Shopify, Squarespace, Adobe Photoshop, Lightroom, and Adobe XD
+                            <br /><br />
+
+                            In my career, I have designed and developed many websites from many niches. These projects have included extensive customization along with the addition of new features.
+                            <br /><br />
+                            Clear communication, honesty, and quick project turnaround are just a few things that are very important to me.</p>
+                        <button className="button-1">My Fiverr Profile</button>
+                    </div>
+                    <div className='who-am-i-2'>
+                        <img src="https://www.imjuwel.com/wp-content/uploads/2023/03/FB_IMG_1676887608505-1024x1024.jpg" className='img' alt="" />
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default WhoAnI
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroSection from '../Component/Navbar/HeroSection/HeroSection'
 import Navbar from '../Component/Navbar/Navbar'
 import Service from '../Component/Service/Service'
+import WhoAnI from '../Component/WhoAmI/WhoAnI'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Navbar />
       <HeroSection />
       <Service />
+      <WhoAnI />
       <h1>home</h1>
  </div>
   )
